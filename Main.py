f911e13bb36d7300481fd87e6de5afd4d5e27cde
saidiya1021 <109952168+saidiya1021@users.noreply.github.com>
2022-09-12 15:29:48 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from typing import List
 def selectionSort(array, size) -> List[int]:
   for i in range (len(array)):
     min=i
-    for j in range(i+1,len(array):
+    for j in range(i+1,len(array)):
       if array[min]>array[j]:
         min=j
     array[i]=array[min],array[min]=array[i]
